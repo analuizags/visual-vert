1546904f84d73577da0960e99d9faa664f2940e4
analuizags <gs.analuiza@gmail.com>
2020-09-07 13:55:10 -0300

    :sparkles: Adição da barra superior no cabelhaço

--- public/header.php
+++ public/header.php
@@ -18,3 +18,31 @@
     <title>Verticalização IF Goiano</title>
 </head>
 <body>
+
+    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 titulo-site" href="index.php">
+            <span data-feather="bar-chart-2"></span>
+            Visual-vert
+        </a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <ul class="navbar-nav px-3 ml-auto">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="glossario.php">
+                    <span data-feather="book" class="mr-1"></span>
+                    Glossário
+                </a>
+            </li>
+        </ul>
+
+        <ul class="navbar-nav px-3 ">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="contato.php">
+                    <span data-feather="at-sign" class="mr-1">></span>
+                    Contato
+                </a>
+            </li>
+        </ul>
+    </nav>
--- public/index.php
+++ public/index.php
@@ -25,34 +25,6 @@
     $anoMin = min(array_column($periodos, 'anoLetInicio'));
 ?>
 
-<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 titulo-site" href="#">
-        <span data-feather="bar-chart-2"></span>
-        Visual-vert
-    </a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <ul class="navbar-nav px-3 ml-auto">
-        <li class="nav-item text-nowrap">
-            <a class="nav-link" href="glossario.php">
-                <span data-feather="book" class="mr-1"></span>
-                Glossário
-            </a>
-        </li>
-    </ul>
-
-    <ul class="navbar-nav px-3 ">
-        <li class="nav-item text-nowrap">
-            <a class="nav-link" href="#">
-                <span data-feather="at-sign" class="mr-1">></span>
-                Contato
-            </a>
-        </li>
-    </ul>
-</nav>
-
 <div class="container-fluid">
     <div class="row">
         <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
