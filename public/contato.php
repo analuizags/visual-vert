027a1fe8e20305fc6dc3b87742ea15cc1a31e660
analuizags <gs.analuiza@gmail.com>
2020-09-07 14:10:39 -0300

    :newspaper: Arquivo de sobre

---
+++ public/contato.php
@@ -0,0 +1,55 @@
+<?php
+    require_once 'header.php';
+?>
+
+<div class="container-fluid">
+    <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse pl-2">
+            <div class="sidebar-sticky pt-3 mt-3">
+                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-2 text-muted">
+                    <span>Desenvolvimento</span>
+                </h6>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#programadora">
+                            <span data-feather="code"></span>
+                            Programadora
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projeto">
+                            <span data-feather="edit-3"></span>
+                            Projeto
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#grupo">
+                            <span data-feather="users"></span>
+                            Grupo
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contato">
+                            <span data-feather="at-sign"></span>
+                            Contato
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4 mb-5">
+            <!-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-4 border-bottom">
+                <h1 class="h2 titulo">Glossário</h1>
+                <div class="btn-toolbar mb-2 mb-md-0">
+                </div>
+            </div> -->
+            
+            
+        </main>
+    </div>
+</div>
+
+<?php
+    require_once 'footer.php';
+?>
